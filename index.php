ece6570bf73ebb7bb873016b01af71049e4cca34
SophieeV <sophievesin@wanadoo.fr>
2021-06-15 15:22:57 +0200

    ajout fichier

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExerciceG1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Formulaire</h1>
+    <form method="post" action="index2.php">
+        <label for="name">Nom</label>
+             <input type="text" id="name" name="nom" placeholder="votre nom" required></br>
+        <label for="surname">Prénom</label>
+            <input type="text" id="surname" name="prenom"placeholder="votre prénom" required></br>
+      <button type="submit" onclick="sendInfos()">envoyer</button>
+    </form>
+    <script>
+function sendInfos(){
+  const name = document.getElementById('name').value;
+  var surname = document.getElementById('surname').value;
+  confirm('informations remplies: prénom: ' + surname + '\n' + 'nom: ' + name);
+};
+    </script>
+
+
+</body>
+</html>
